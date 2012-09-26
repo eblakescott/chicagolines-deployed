4a6627870ab8c0db3083460a366071375f7dca16
Ed Scott <eblakescott@gmail.com>
2012-09-26 11:35:34 -0500

    initial commit of new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chicagolines::Application.config.secret_token = '25bc8637e9d8bd60631f8c2372d117d5747b581324f9e919278577d31ae89c6f97eaf98113374ed456ee0c4449f4669cf6690f6acd8be900a136cff5f0a37704'
