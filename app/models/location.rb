af61ad875432c789ebe1576a8b8da52313033da4
Ed Scott <eblakescott@gmail.com>
2012-09-26 12:31:25 -0500

    added validation requirements for Locaiton model: name, description, image_url and category must be present; name must be unique; image_url must be a gif, jpg, or png file

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,10 @@
 class Location < ActiveRecord::Base
   attr_accessible :category, :description, :image_url, :name
+  validates :category, :description, :image_url, :name, presence: true
+  validates :name, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with:
+    %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
