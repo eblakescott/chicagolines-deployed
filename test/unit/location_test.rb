781817887a9dc35dfdc2e2343c8be7a63e37975a
Ed Scott <eblakescott@gmail.com>
2012-09-26 17:37:35 -0500

    Added locations test data to fixtures file; added more tests to locations unit test file

--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -10,4 +10,46 @@ assert location.errors[:description].any?
 assert location.errors[:category].any?
 end
 
+def new_location(image_url)
+    Location.new(image_url:   image_url,
+		name:       "My Location",
+                description: "yyy",
+                category:       "mmm")
+end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    
+    ok.each do |name|
+      assert new_location(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_location(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test "location is not valid without a unique name" do
+    location = Location.new(image_url:   "fred.gif",
+			  name:       	 locations(:ruby).name,
+                          description: "yyy", 
+                          category:    "mmm")
+
+    assert location.invalid?
+    assert_equal ["has already been taken"], location.errors[:name]
+  end
+
+  test "location is not valid without a unique name - i18n" do
+    location = Location.new(image_url:   "fred.gif",
+			  name:        locations(:ruby).name,
+                          description: "yyy", 
+                          category:    "mmm")
+
+    assert location.invalid?
+    assert_equal [I18n.translate('activerecord.errors.messages.taken')],
+                 location.errors[:name]
+  end
+
 end
