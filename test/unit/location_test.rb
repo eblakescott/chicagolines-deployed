5c5c60c564f5ffbc72cb15982b583180313de009
Ed Scott <eblakescott@gmail.com>
2012-09-26 17:00:15 -0500

    added unit test for Location; ran rake test:units

--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+test "location attributes must not be empty" do
+location = Location.new
+assert location.invalid?
+assert location.errors[:image_url].any?
+assert location.errors[:name].any?
+assert location.errors[:description].any?
+assert location.errors[:category].any?
+end
+
 end
