9d18a96f08a0a859f8bc72f70d960f3633c57c59
Ed Scott <eblakescott@gmail.com>
2012-09-28 16:47:31 -0500

    added has_many to location model; belongs_to to wait model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,6 @@
 class Location < ActiveRecord::Base
   attr_accessible :category, :description, :image_url, :name
+  has_many :waits
   validates :category, :description, :image_url, :name, presence: true
   validates :name, uniqueness: true
   validates :image_url, allow_blank: true, format: {
--- app/models/wait.rb
+++ app/models/wait.rb
@@ -1,3 +1,4 @@
 class Wait < ActiveRecord::Base
   attr_accessible :duration, :location_id
+  belongs_to :location
 end
