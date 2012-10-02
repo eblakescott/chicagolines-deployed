cd47b6b2a31632e82de1759fd6f25dda07e80f12
Ed Scott <eblakescott@gmail.com>
2012-10-02 14:34:28 -0500

    added dependent: :destroy to locationsmodel

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ActiveRecord::Base
   attr_accessible :category, :description, :image_url, :name
-  has_many :waits
+  has_many :waits, dependent: :destroy
   validates :category, :description, :image_url, :name, presence: true
   validates :name, uniqueness: true
   validates :image_url, allow_blank: true, format: {
