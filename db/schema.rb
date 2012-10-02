b6f9332d6d7a2859982a79bec5b3d55324a6ef19
Ed Scott <eblakescott@gmail.com>
2012-10-02 12:54:58 -0500

    ran Pages migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120926164612) do
+ActiveRecord::Schema.define(:version => 20121002175329) do
 
   create_table "locations", :force => true do |t|
     t.string   "image_url"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(:version => 20120926164612) do
     t.string   "category"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "pages", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "waits", :force => true do |t|
