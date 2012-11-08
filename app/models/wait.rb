141792622d2b2b807d4c2cec33169feac536105b
Ed Scott <eblakescott@gmail.com>
2012-11-07 20:25:51 -0600

    fixed date time format to mm dd yy time

--- app/models/wait.rb
+++ app/models/wait.rb
@@ -1,5 +1,10 @@
 class Wait < ActiveRecord::Base
   attr_accessible :duration, :location_id
   belongs_to :location
+
+#See http://techoctave.com/c7/posts/23-rails-date-formats
+  def my_time
+    updated_at.strftime("%B %d %Y at %I:%M")
+  end
 end
 
